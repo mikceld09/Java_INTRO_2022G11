96b2c9b7c0509e30f3af139ca3738b0ef8415c7d
mikceld09 <mikael.celdran@gmail.com>
2022-02-05 09:18:10 +0800

    Initial commit

---
+++ src/ProgrammingBuildingBlocks.java
@@ -0,0 +1,14 @@
+public class ProgrammingBuildingBlocks {
+}
+
+import java.util.Scanner;
+
+public class DataInput
+{
+    public static void main( String [] args )
+    {
+        Scanner scan = new Scanner ( System.in );
+
+
+        System.out.print( "\Enter"
+    }
